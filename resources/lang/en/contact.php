e3e866c1b6e1ef2479618f80a4e23bb746372eca
Ahmad Saleh <ahmadmsa87@gmail.com>
2017-07-13 17:37:59 +0300

    Update contact message

--- resources/lang/en/contact.php
+++ resources/lang/en/contact.php
@@ -12,9 +12,10 @@ return [
     | these language lines according to your application's requirements.
     |
     */
+    'home' => 'Home Page',
     'contact_us' => 'Contact us',
     'create_book' =>'CREATE BOOK »',
-    'contact_message' => 'If you have any question. Don`t hesitate! please contact us now. ',
+    'contact_message' => 'If you have any question, please do not hesitate to contact us immediately.',
     'footer' => 'PastBook© by Ahmad Saleh',
 
 ];
