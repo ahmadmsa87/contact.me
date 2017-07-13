23f538c1d0198ed4a3f6466c5d12e80783e0fa77
Ahmad Saleh <ahmadmsa87@gmail.com>
2017-07-13 16:34:27 +0300

    Add head section of template

---
+++ resources/views/components/head.blade.php
@@ -0,0 +1,25 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+    <title>Contact Us</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="<?= URL::to('/css/w3.css') ?>">
+    <link rel="stylesheet" href="<?= URL::to('/css/w3-theme-blue-grey.css') ?>">
+    <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Open+Sans'>
+    <link rel="stylesheet" href="<?= URL::to('/css/font-awesome.min.css') ?>">
+    <link rel="stylesheet" href="<?= URL::to('/css/main.css') ?>">
+    <link rel="stylesheet" href="<?= URL::to('/css/style.css') ?>">
+    <link rel="stylesheet" href="<?= URL::to('/bootstrap/css/bootstrap.min.css') ?>">
+    <link rel="shortcut icon"  type="image/png"  href="<?= URL::to('/image/favicon.ico') ?>">
+    <script src="<?= URL::to('/js/jquery-3.1.0.min.js') ?>"></script>
+    <style>
+
+
+    </style>
+
