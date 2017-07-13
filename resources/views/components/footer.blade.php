183a13a8a524410bfac198b7656bfdfe0e1199f9
Ahmad Saleh <ahmadmsa87@gmail.com>
2017-07-13 16:33:25 +0300

    Add footer section template

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,22 @@
+<?php
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+<footer class="w3-container w3-white w3-center footer_last ">
+    <p> @lang('contact.footer')</p>
+</footer>
+
+<script>
+    // Used to toggle the menu on smaller screens when clicking on the menu button
+    function openNav() {
+        var x = document.getElementById("navDemo");
+        if (x.className.indexOf("w3-show") == -1) {
+            x.className += " w3-show";
+        } else {
+            x.className = x.className.replace(" w3-show", "");
+        }
+    }
+</script>
